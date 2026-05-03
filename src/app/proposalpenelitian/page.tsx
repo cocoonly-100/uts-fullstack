79f5e9f9e607754aaafddd9068ad9c3ecd10f60a
jude <judewilliam949@gmail.com>
2026-05-03 18:49:02 +0700

    Add files via upload

---
+++ src/app/proposalpenelitian/page.tsx
@@ -0,0 +1,10 @@
+import "@/app/globals.css";
+export default function Home() {
+  return (
+    <main>
+      <p className='header' style={{marginTop:'1vh'}}>PROPOSAL PENELITIAN</p>
+      <button> Panduan Proposal Penelitian Revisi 29 April 2019</button>
+    </main>
+  );
+}
+

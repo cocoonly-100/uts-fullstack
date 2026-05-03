c73a5ff4c5f47edfca589fc23331e3c0b8f6748f
jude <judewilliam949@gmail.com>
2026-05-03 18:53:42 +0700

    Add files via upload

---
+++ src/app/laporanpenelitian/page.tsx
@@ -0,0 +1,10 @@
+import "@/app/globals.css";
+export default function Home() {
+  return (
+    <main>
+      <p className='header' style={{marginTop:'1vh'}}>LAPORAN PENELITIAN</p>
+      <button> Panduan LAPORAN Penelitian Revisi 29 April 2019</button>
+    </main>
+  );
+}
+

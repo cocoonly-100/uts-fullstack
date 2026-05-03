b08a24d9939a731d4b2c062895d7e1e4b48da6fa
jude <judewilliam949@gmail.com>
2026-05-03 22:46:33 +0700

    Add files via upload

---
+++ src/app/diseminasipenelitian/page.tsx
@@ -0,0 +1,13 @@
+import "@/app/globals.css";
+export default function Home() {
+  return (
+    <main>
+      <p className='header' style={{marginTop:'1vh'}}>DISEMINASI PENELITIAN</p>
+      <div style={{display:'flex'}}>
+      <button> Panduan diseminasi Penelitian Revisi 29 April 2019</button>
+      <button> Panduan Aturan Pelaksanaan Diseminasi Penelitian Revisi 29 April 2019</button>
+      </div>
+    </main>
+  );
+}
+

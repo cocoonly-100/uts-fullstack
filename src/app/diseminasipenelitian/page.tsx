7a846b56f5d1e89195d251076da9acaaeff933a9
jude <judewilliam949@gmail.com>
2026-05-09 20:48:23 +0700

    page.tsx

--- src/app/diseminasipenelitian/page.tsx
+++ src/app/diseminasipenelitian/page.tsx
@@ -1,6 +1,6 @@
 import "@/app/globals.css";
 import { metadatagenerator } from "../metadata";
-export const metadata=metadatagenerator"diseminasipenelitian"
+export const metadata=metadatagenerator("diseminasipenelitian")
 export default function Home() {
   return (
     <main>
